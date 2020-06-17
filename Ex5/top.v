7e6dabcd8414017033cb43f3e2b30f42d0ea1669
KoceanZHENG <53459099+KoceanZHENG@users.noreply.github.com>
2020-06-17 12:31:00 +0100

    Update top.v

--- Ex5/top.v
+++ Ex5/top.v
@@ -13,3 +13,25 @@
 //           red, amber, green
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns/100ps
+
+module traffic_light(
+//Todo: add ports
+input clk;
+output reg red,amber,green;
+)
+//Todo: add wire
+wire[2:0] rag = {red,amber,green};
+
+//Todo: add user logic
+always @ (posedge clk) 
+begin
+	case(rag)
+	3b'100: rag<=110;
+	3b'110: rag<=001;
+	3b'001: rag<=010;
+	3b'010: rag<=100;
+	default: rag<=100;
+	endcase
+end
+endmodule
