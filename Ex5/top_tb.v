a1f761c39a792b1283123a78e82e162edab7e337
KoceanZHENG <53459099+KoceanZHENG@users.noreply.github.com>
2020-06-17 14:22:05 +0100

    Update top_tb.v

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,3 +7,56 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns / 100ps
+
+module top_tb(
+);
+//Parameters
+	parameter CLK_PERIOD = 10;
+
+//Registers and wires
+	reg clk;
+	wire [2:0] rag;
+
+//Clock generation
+	initial
+	begin
+	clk = 1;
+	forever
+	#(CLK_PERIOD/2) clk = ~clk;
+	end
+
+//User logic
+initial
+begin
+	err = 0;
+
+	#5
+	forever begin
+	#5
+	//Testing unwanted states
+	if(rag != 3'b100 || rag != 3'b110 || rag != 3'b001 || rag != 3'b010)
+	$display("Unwanted States!");
+	err = 1;
+	end
+end
+
+//Finish test, check for success
+initial
+begin
+	#200
+	if(err = 0)
+	$display("Test Passed!");
+	$finish
+end
+
+//Instantiate dice module
+traffic_light top(
+.clk(clk),
+.red(red),
+.amber(amber),
+.green(green)
+);
+
+endmodule
